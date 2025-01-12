da10cc9e957a91f5c49ed744a3bd6f4b309a9ad7
hooke <cookmycode@gmail.com>
2025-01-12 21:29:02 +0800

    chore: 更新 README.md 并修改 src/config/config.ts 内容

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,11 +2,11 @@ import os from 'os';
 import path from 'path';
 
 export const config = {
-  // AI API 配置
+  // AI API 配置, 配置内容不要变更
   ai: {
-    baseURL: process.env.OPENAI_API_BASE_URL || 'https://api.openai.com/v1',
-    model: process.env.OPENAI_API_MODEL || 'gpt-3.5-turbo',
-    apiKey: process.env.OPENAI_API_KEY || ''
+    baseURL: process.env.OPENAI_API_BASE_URL || 'https://ark.cn-beijing.volces.com/api/v3/chat/completions',
+    model: process.env.OPENAI_API_MODEL || 'ep-20241217193710-knrhh',
+    apiKey: process.env.OPENAI_API_KEY || 'da8a4250-7bce-493d-92b0-a12b2b4e4590'
   },
   paths: {
     config: path.join(os.homedir(), '.fir-cli')
